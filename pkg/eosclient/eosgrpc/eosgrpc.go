e09880501f756efadd6294cbb7682692817b25b5
Fabrizio Furano <fabrizio.furano@gmail.com>
2021-02-24 11:01:56 +0100

    Fix comment

--- pkg/eosclient/eosgrpc/eosgrpc.go
+++ pkg/eosclient/eosgrpc/eosgrpc.go
@@ -127,7 +127,7 @@ type Client struct {
 	cl     erpc.EosClient
 }
 
-// GetHttpCl: Create an http client for immediate usage, using the already instantiated resources
+// GetHttpCl creates an http client for immediate usage, using the already instantiated resources
 func (c *Client) GetHttpCl() *ehttp.EosHttpClient {
 	return ehttp.New(&c.opt.httpopts)
 }
